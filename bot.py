256fe5402ea0f4f0d7b560eb16ab79d95ae9fe24
mrvasil <alexeyka.v@yandex.ru>
2025-01-04 22:52:50 +0300

    Update bot functionality: change CHANNEL_ID, modify welcome message, enhance user interaction with inline buttons, and improve message handling logic for forwarding and copying messages

--- bot.py
+++ bot.py
@@ -6,12 +6,13 @@ API_TOKEN = '7983118789:AAE_oZyr-JhlK6DyaotZ4LS-P1jtFv5j980'
 bot = telebot.TeleBot(API_TOKEN)
 
 GROUP_ID = -1002485954656
-CHANNEL_ID = -1002377166835
+#CHANNEL_ID = -1002377166835
+CHANNEL_ID = -1002385851046
 my_id = bot.get_me().id
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Привет! Вы можете отправлять мне сообщения с текстом, видео и фото.")
+    bot.send_message(message.chat.id, "Привет! Вы можете отправлять мне сообщения с текстом или любым медиа.")
 
 @bot.message_handler(func=lambda message: True, content_types=['audio', 'photo', 'voice', 'video', 'document',
     'text', 'location', 'contact', 'sticker', 'animation', 'poll'])
@@ -20,38 +21,38 @@ def uzhimatel(message):
         bot.delete_message(GROUP_ID, message.id)
         return
 
-
-    print("MASSIVE")
     user_info = f"Отправитель: {message.from_user.first_name} (@{message.from_user.username})"
+    print('[#] '+user_info)
     mid = bot.forward_message(GROUP_ID, message.chat.id, message.id).id
 
     markup = types.InlineKeyboardMarkup()
-    btn_send = types.InlineKeyboardButton("Отправить", callback_data=json.dumps({"send": 1, "mid": mid, "ochat":message.chat.id, "omsg":message.id}))
-    btn_cancel = types.InlineKeyboardButton("Не отправлять", callback_data=json.dumps({"send": 0, "mid": mid, "ochat":message.chat.id, "omsg":message.id}))
+    btn_send = types.InlineKeyboardButton("Указать имя", callback_data=json.dumps({"a": 1, "c":message.chat.id, "m":message.id, "i": mid}))
+    btn_cancel = types.InlineKeyboardButton("Не указывать", callback_data=json.dumps({"a": 0, "c":message.chat.id, "m":message.id, "i": mid}))
     markup.add(btn_send, btn_cancel)
 
-    bot.send_message(GROUP_ID, user_info, reply_markup=markup)
-
-
-    
-
+    bot.send_message(message.chat.id, "Указывать ли имя при отправке в канал?", reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
     d = json.loads(call.data)
-    if d["send"]:
-        bot.copy_message(CHANNEL_ID, d["ochat"], d["omsg"])
-        
-        bot.delete_message(GROUP_ID, call.message.id)
-        bot.delete_message(GROUP_ID, d["mid"])
-        bot.send_message(d["ochat"], "Сообщение отправлено", reply_to_message_id=d["omsg"])
-
+    if "send" in d:
+        if d["send"]==1:
+            if d["a"]==1:
+                bot.forward_message(CHANNEL_ID, d["c"], d["m"])
+            else:
+                bot.copy_message(CHANNEL_ID, d["c"], d["m"])
+            bot.delete_message(GROUP_ID, call.message.id)
+            bot.send_message(d["c"], "Сообщение отправлено", reply_to_message_id=d["m"])
+        else:
+            bot.delete_message(GROUP_ID, call.message.id)
+            bot.send_message(d["c"], "Сообщение отклонено", reply_to_message_id=d["m"])
     else:
-        bot.delete_message(GROUP_ID, call.message.id)
-        bot.delete_message(GROUP_ID, d["mid"])
-        bot.send_message(d["ochat"], "Сообщение отклонено", reply_to_message_id=d["omsg"])
-
-
-
+        bot.send_message(d["c"], "Спасибо за контент! Отправил загипычам на модерацию =)")
+        markup = types.InlineKeyboardMarkup()
+        btn_send = types.InlineKeyboardButton("Отправить", callback_data=json.dumps({"send": 1, "c":d["c"], "m":d["m"], "a":d["a"]}))
+        btn_cancel = types.InlineKeyboardButton("Не отправлять", callback_data=json.dumps({"send": 0, "c":d["c"], "m":d["m"], "a":d["a"]}))
+        markup.add(btn_send, btn_cancel)
+        
+        bot.send_message(GROUP_ID, "Отправить в канал?", reply_markup=markup)
 
 bot.polling(non_stop=True)
